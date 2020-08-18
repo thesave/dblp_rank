67634caf204eb5bad557b06ffcdec6762598de0a
thesave <Thesave86@gmail.com>
2020-08-18 09:57:47 +0200

    Update dblp_rank.user.js

--- dblp_rank.user.js
+++ dblp_rank.user.js
@@ -2,7 +2,7 @@
 // @name            Rank DBLP
 // @namespace       https://github.com/thesave
 // @description     Add ranking of conferences (from GII-GRIN-SCIE) and journal (from SCIMAGO) to a DBLP researcher's page
-// @version         0.4.4
+// @version         0.4.5
 // @license         MIT
 // @copyright       2018+
 // @icon            https://dblp.uni-trier.de/img/favicon.ico
